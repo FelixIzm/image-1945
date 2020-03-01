166e957fd951809101dd75d7aaf39cecab067c67
FelixIzm <for_cv@mail.ru>
2020-03-01 20:48:47 +0300

    'heroku'

--- get/views.py
+++ get/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import UserForm
-import os, tempfile
+import os, tempfile, requests
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -11,7 +11,12 @@ def index(request):
     image_id=0
     userform = UserForm({'image_id':image_id})
     image_id = request.POST.get("image_id")
+    url = 'https://www.facebook.com/favicon.ico'
+    r = requests.get(url, allow_redirects=True)
+
     dirpath = tempfile.mkdtemp()
+    
+    open(os.path.join(dirpath, 'facebook.ico'), 'wb').write(r.content)
     return render(request, "get/index.html", {"form": userform,"web_link": dirpath})
 
 # Create your views here.
