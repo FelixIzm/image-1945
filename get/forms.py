c6d4488e8de5305bcc30b8f79a64d677d594a9da
FelixIzm <for_cv@mail.ru>
2020-03-29 20:03:40 +0300

    'amazon'

--- get/forms.py
+++ get/forms.py
@@ -1,10 +1,9 @@
 from django import forms
 
 class UserForm(forms.Form):
-    image_id = forms.CharField(label='ID',initial='1234',widget= forms.TextInput(attrs={'class':'myfield'}))
-    excel = forms.BooleanField(required = False)
+    image_id = forms.CharField(label='ID', initial='1234', widget=forms.TextInput(attrs={'class':'myfield'}))
+    excel = forms.BooleanField(required=False)
 #,widget= forms.TextInput(attrs={'class':'field-style','id':'some_id'})
 class FormSelectDir(forms.Form):
     #path_dir = forms.FilePathField(path='/tmp',allow_files=False,allow_folders=True)
-    path_dir = forms.CharField(label='path',initial='',required=False,widget= forms.TextInput(attrs={'class':'myfield'}))
-
+    path_dir = forms.CharField(label='path', initial='', required=False, widget=forms.TextInput(attrs={'class':'myfield'}))
