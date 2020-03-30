10ec8e900706e93dc7f9bd2e598433dc1e0fb3f5
FelixIzm <for_cv@mail.ru>
2020-03-30 18:44:31 +0300

    'amazon'

--- get/forms.py
+++ get/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class FormSelectId(forms.Form):
-    _id = forms.CharField(label='ID image', initial='123', required=False, widget=forms.TextInput(attrs={'class':'myfield'}))
+    _id = forms.CharField(label='ID image', initial='0', required=False, widget=forms.TextInput(attrs={'class':'myfield'}))
     #path_dir = forms.CharField(label='path', initial='', required=False, widget=forms.TextInput(attrs={'class':'myfield'}))
